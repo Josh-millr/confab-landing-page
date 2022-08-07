9b8381ce781e4954938f175bb4a8b6532d04c953
josh-miller <joshuae.miller100@gmail.com>
2022-08-07 20:45:02 +0100

    Done with the newEvent section

--- src/shared/components/sections/CreateEvent.jsx
+++ src/shared/components/sections/CreateEvent.jsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
 import { Button } from "@components/index";
 import { RiAddFill } from "react-icons/ri";
 
 export const CreateEvent = () => {
   return (
-    <div className="grid grid-rows-1 py-[48px]">
-      <div className="grid grid-cols-1 grid-rows-1 gap-y-[56px] px-pageMargin-sm sm:grid-cols-2 sm:px-pageMargin-md lg:px-pageMargin-lg">
+    <div className="">
+      {/* Column 1 */}
+      <div className="grid grid-cols-1 grid-rows-1 gap-y-[56px] py-[60px] px-pageMargin-sm sm:grid-cols-2 sm:px-pageMargin-md md:py-[80px] lg:px-pageMargin-lg">
         {/* Title */}
         <h1 className="font-heading text-h2 text-black md:text-h1">
           host
@@ -12,7 +14,7 @@ export const CreateEvent = () => {
           <span className="text-opal">for free</span>
         </h1>
         {/* Description */}
-        <div className="grid grid-cols-1 grid-rows-1 gap-y-[24px]">
+        <div className="grid grid-cols-1 grid-rows-1 gap-y-[24px] sm:items-end sm:justify-self-end">
           <Button label="New event" icon={RiAddFill} type="stroke" size="md" />
           <p className="max-w-[400px] font-body text-body-sm text-black">
             Virtual meetings, and conferences, anyone can share, join or host
@@ -20,8 +22,14 @@ export const CreateEvent = () => {
           </p>
         </div>
       </div>
-      {/* For Banner */}
-      <div className=""></div>
+      {/* Column 2 */}
+      <div className="relative left-[-30%] h-[554px] w-[1064px] sm:left-[-25%] md:left-[-10%] lg:left-[0] lg:h-[665px] lg:w-[auto]">
+        <Image
+          src="/illustrations/banner.svg"
+          alt="image of many people standing"
+          layout="fill"
+        />
+      </div>
     </div>
   );
 };
