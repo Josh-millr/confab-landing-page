d98556466095e187f265aa9a52ff2053f18e00be
josh-miller <joshuae.miller100@gmail.com>
2022-08-12 20:02:26 +0100

    Created the step switcher animation

--- src/shared/components/sections/MainSteps.jsx
+++ src/shared/components/sections/MainSteps.jsx
@@ -1,49 +1,23 @@
+import React, { useState, useEffect } from "react";
 import { ActivityCard } from "@components/index";
 
 export const MainSteps = ({}) => {
-  const activities = [
-    {
-      key: 9712,
-      title: "Become a host",
-      para1: "Anyone can",
-      paraHighlight: "Become a host",
-      para2: "and customize the number of participants.",
-      img: "/illustrations/man-with-sweater.svg",
-      active: true,
-    },
-    {
-      key: 12312,
-      title: "plan ahead",
-      para1: "Click",
-      paraHighlight: "New Event",
-      para2: "to schedule meetings or conferences and send invites.",
-      img: "/illustrations/woman-with-knife.svg",
-      active: false,
-    },
-    {
-      key: 213,
-      title: "get a link, share.",
-      para1: "Click",
-      paraHighlight: "New Event",
-      para2: "to get a link you can send to people you want to invite.",
-      img: "/illustrations/woman-with-mobile.svg",
-      active: false,
-    },
-  ];
-  let activityList = activities.map(
-    ({ key, title, para1, paraHighlight, para2, img, active }) => (
-      <ActivityCard
-        key={key}
-        img={img}
-        heading={title}
-        paraStart={para1}
-        paraEnd={para2}
-        paraHighlight={paraHighlight}
-        flow="center"
-        active={active}
-      />
-    )
-  );
+  let [stepIncrement, setStepIncrement] = useState(1);
+  let [firstStep, setFirstStep] = useState(true);
+  let [secondStep, setSecondStep] = useState(false);
+  let [thirdStep, setThirdStep] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      let incrementedStep = stepIncrement != 3 ? stepIncrement + 1 : 1 ;
+      setFirstStep(stepIncrement == 1 ? true : false)
+      setSecondStep(stepIncrement == 2 ? true : false)
+      setThirdStep(stepIncrement == 3 ? true : false)
+      setStepIncrement(incrementedStep);
+
+    }, 2000);
+  }, [stepIncrement]);
+
   return (
     <section>
       <div className="grid grid-rows-1 gap-y-[64px] bg-cloudy px-pageMargin-sm py-[52px] sm:px-pageMargin-md md:py-[96px] lg:gap-y-[48px] lg:px-pageMargin-lg">
@@ -60,7 +34,33 @@ export const MainSteps = ({}) => {
         </header>
         {/* Column 2 */}
         <div className="grid grid-cols-1 grid-rows-1 place-content-center gap-y-[16px] sm:grid-cols-3 sm:gap-y-[0px] sm:gap-x-[24px] lg:gap-x-[40px]">
-          {activityList}
+          <ActivityCard
+            img="/illustrations/man-with-sweater.svg"
+            heading="Become a host"
+            paraStart="Anyone can"
+            paraEnd="and customize the number of participants."
+            paraHighlight="Become a host"
+            flow="center"
+            active={firstStep}
+          />
+          <ActivityCard
+            img="/illustrations/woman-with-knife.svg"
+            heading="plan ahead"
+            paraStart="to schedule meetings or conferences and send invites."
+            paraEnd="and customize the number of participants."
+            paraHighlight="New Event"
+            flow="center"
+            active={secondStep}
+          />
+          <ActivityCard
+            img="/illustrations/woman-with-mobile.svg"
+            heading="get a link, share."
+            paraStart="Click"
+            paraEnd="to get a link you can send to people you want to invite."
+            paraHighlight="New Event"
+            flow="center"
+            active={thirdStep}
+          />
         </div>
       </div>
     </section>
